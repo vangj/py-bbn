971e8f4968f9e28e6308191afb8650fe33d3be8d
Jee Vang <vangjee@gmail.com>
2017-01-19 17:37:06 -0500

    add pip publication information

---
+++ README.md
@@ -0,0 +1,8 @@
+# PyBBN
+
+PyBBN is Python library for Bayesian Belief Networks (BBNs) exact inference using the 
+[junction tree algorithm](https://en.wikipedia.org/wiki/Junction_tree_algorithm) or Probability
+Propagation in Trees of Clusters. The implementation is taken directly from [C. Huang and A. Darwiche, "Inference in
+Belief Networks: A Procedural Guide," in International Journal of Approximate Reasoning, vol. 15,
+pp. 225--263, 1999](http://pages.cs.wisc.edu/~dpage/ijar95.pdf).
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybbn',
+    version='0.0.1',
+    author='Jee Vang',
+    author_email='vangjee@gmail.com',
+    packages=find_packages(),
+    description='Exact inference in Bayesian Belief Networks using Probability Propagation in Trees of Clusters (PPTC)',
+    url='https://github.com/vangj/py-bbn',
+    keywords=['bayesian', 'belief', 'network', 'exact', 'inference', 'junction', 'tree', 'algorithm', 'pptc', 'dag'],
+    classifiers=[]
+)
