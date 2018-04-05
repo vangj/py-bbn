4524be65341a5854de0c3e3cd049a240fdd1da95
Jee Vang <vangjee@gmail.com>
2018-04-05 00:43:45 -0400

    change description

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
-    description='Exact inference in Bayesian Belief Networks using Probability Propagation in Trees of Clusters (PPTC)',
+    description='Inference in Bayesian Belief Networks using Probability Propagation in Trees of Clusters (PPTC) and Gibbs sampling',
     url='https://github.com/vangj/py-bbn',
-    keywords=['bayesian', 'belief', 'network', 'exact', 'inference', 'junction', 'tree', 'algorithm', 'pptc', 'dag'],
+    keywords=['bayesian', 'belief', 'network', 'exact', 'approximate', 'inference', 'junction', 'tree', 'algorithm',
+              'pptc', 'dag', 'gibbs', 'sampling', 'multivariate', 'conditional', 'gaussian', 'linear'],
     classifiers=[]
 )
