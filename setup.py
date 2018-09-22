12aa7d086a5d8fcdaf25e2c485758f2b0e454ca0
Jee Vang, Ph.D <vangjee@gmail.com>
2018-09-22 01:30:30 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybbn',
-    version='0.0.6',
+    version='0.0.7',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
