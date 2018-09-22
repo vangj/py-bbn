d2334af534a28339d85c8345ef609cc4908a406e
Jee Vang, Ph.D <vangjee@gmail.com>
2018-09-22 14:30:46 -0400

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setup(
     name='pybbn',
-    version='0.1.0',
+    version='0.1.1',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
