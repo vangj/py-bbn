916f6b1ea21504bea1b5a2196331076eaf4e6c75
Jee Vang, Ph.D <vangjee@gmail.com>
2018-09-22 01:48:30 -0400

    add classifiers + description; modify short description

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_desc = fh.read()
+
 setup(
     name='pybbn',
-    version='0.0.7',
+    version='0.0.8',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
-    description='Inference in Bayesian Belief Networks using Probability Propagation in Trees of Clusters (PPTC) and Gibbs sampling',
+    description='Learning and Inference in Bayesian Belief Networks',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
     url='https://github.com/vangj/py-bbn',
     keywords=['bayesian', 'belief', 'network', 'exact', 'approximate', 'inference', 'junction', 'tree', 'algorithm',
-              'pptc', 'dag', 'gibbs', 'sampling', 'multivariate', 'conditional', 'gaussian', 'linear'],
-    classifiers=[]
+              'pptc', 'dag', 'gibbs', 'sampling', 'multivariate', 'conditional', 'gaussian', 'linear', 'causal',
+              'causality', 'structure', 'parameter'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 4 - Beta'
+    ]
 )
