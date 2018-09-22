e597e7392513edd97ca5b5a9d421cdc710852dc2
Jee Vang, Ph.D <vangjee@gmail.com>
2018-09-22 01:57:15 -0400

    upgrade version to see if pypi will render readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pybbn',
-    version='0.0.8',
+    version='0.0.9',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
