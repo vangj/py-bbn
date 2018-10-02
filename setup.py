a8c7aa74279cc12a7279052dce324e3635ad3a81
Jee Vang, Ph.D <vangjee@gmail.com>
2018-10-02 00:18:46 -0400

    fix version requirements

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.rst', 'r') as fh:
 
 setup(
     name='pybbn',
-    version='0.1.1',
+    version='0.1.2',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(),
     description='Learning and Inference in Bayesian Belief Networks',
     long_description=long_desc,
     url='https://github.com/vangj/py-bbn',
-    python_requires='>=2.7, >=3.0',
+    python_requires='>=2.7.0, >=3.0.0',
     keywords=' '.join(['bayesian', 'belief', 'network', 'exact', 'approximate', 'inference', 'junction', 'tree',
                        'algorithm', 'pptc', 'dag', 'gibbs', 'sampling', 'multivariate', 'conditional', 'gaussian',
                        'linear', 'causal', 'causality', 'structure', 'parameter']),
