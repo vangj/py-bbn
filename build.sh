c7a2e6d20b8dacd7435cc9c34f7634309fe7edd0
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-02 01:12:08 -0400

    accept command line args; https://www.lifewire.com/pass-arguments-to-bash-script-2200571

--- build.sh
+++ build.sh
@@ -3,9 +3,26 @@
 DOCKER_FILE=Dockerfile
 DOCKER_REPO=pybbn37
 DOCKER_TAG=local
+APYBBN_VERSION=version
+APYPI_REPO=repo
 
-docker build --no-cache \
+while getopts v:r: option
+do
+  case "${option}"
+  in
+  v) APYBBN_VERSION=${OPTARG};;
+  r) APYPI_REPO=${OPTARG};;
+esac
+done
+
+if [[ "version" == $APYBBN_VERSION ]]; then
+  echo "version is required; -v"
+elif [[ "repo" == $APYPI_REPO ]]; then
+  echo "repo is required; -r"
+else
+  docker build --no-cache \
     -f $DOCKER_FILE \
-    --build-arg APYBBN_VERSION=0.2.4 \
-    --build-arg APYPI_REPO=testpypi \
+    --build-arg APYBBN_VERSION=$APYBBN_VERSION \
+    --build-arg APYPI_REPO=$APYPI_REPO \
     -t ${DOCKER_REPO}:${DOCKER_TAG} .
+fi
