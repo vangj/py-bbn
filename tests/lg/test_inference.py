e54edbf54739a8538412468b0bc25549750c5ccd
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-10 03:45:10 -0400

    add X -> Y test

--- tests/lg/test_inference.py
+++ tests/lg/test_inference.py
@@ -23,6 +23,80 @@ def teardown():
 
 
 @with_setup(setup, teardown)
+def test_two_variables():
+    """
+    Tests inference on X -> Y
+    :return: None.
+    """
+    N = 10000
+    x0 = normal(0, 1, N)
+    x1 = normal(1 + 2 * x0, 1, N)
+
+    X = np.hstack([x0.reshape(-1, 1), x1.reshape(-1, 1)])
+    M = np.mean(X, axis=0)
+    S = np.cov(X.T)
+
+    mvn = MvnGaussian(M, S, N)
+
+    mvn.update_mean_cov(np.array([1.0]), [0])
+
+    M_u, S_u = mvn.get_params()
+    M_e = np.array([1., 5.00664999])
+    S_e = np.array([[0.01, 1.98924418],
+                    [1.98924418, 1.00995594]])
+
+    assert (M_e.shape[0] == M_u.shape[0])
+    for m, m_u in zip(M_e, M_u):
+        assert_almost_equal(m, m_u, delta=0.1)
+
+    assert (S_e.shape[0] == S_u.shape[0])
+    assert (S_e.shape[1] == S_u.shape[1])
+
+    rows, cols = S_e.shape
+    for r in range(rows):
+        for c in range(cols):
+            assert_almost_equal(S_e[r, c], S_u[r, c], delta=0.5)
+
+    mvn.update_mean_cov(np.array([1.0]), [1])
+
+    M_u, S_u = mvn.get_params()
+    M_e = np.array([0.40151428, 1.])
+    S_e = np.array([[0.19994501, 1.98924418],
+                    [1.98924418, 0.01]])
+
+    assert (M_e.shape[0] == M_u.shape[0])
+    for m, m_u in zip(M_e, M_u):
+        assert_almost_equal(m, m_u, delta=0.1)
+
+    assert (S_e.shape[0] == S_u.shape[0])
+    assert (S_e.shape[1] == S_u.shape[1])
+
+    rows, cols = S_e.shape
+    for r in range(rows):
+        for c in range(cols):
+            assert_almost_equal(S_e[r, c], S_u[r, c], delta=0.5)
+
+    mvn.update_mean_cov(None, None)
+
+    M_u, S_u = mvn.get_params()
+    M_e = np.array([0.00172341, 0.99427581])
+    S_e = np.array([[0.99070024, 1.98924418],
+                    [1.98924418, 5.00419378]])
+
+    assert (M_e.shape[0] == M_u.shape[0])
+    for m, m_u in zip(M_e, M_u):
+        assert_almost_equal(m, m_u, delta=0.1)
+
+    assert (S_e.shape[0] == S_u.shape[0])
+    assert (S_e.shape[1] == S_u.shape[1])
+
+    rows, cols = S_e.shape
+    for r in range(rows):
+        for c in range(cols):
+            assert_almost_equal(S_e[r, c], S_u[r, c], delta=0.5)
+
+
+@with_setup(setup, teardown)
 def test_serial_inference():
     """
     Tests inference on a serial structure X -> Y -> Z.
@@ -42,7 +116,7 @@ def test_serial_inference():
     M_u, S_u = mvn.get_params()
 
     M_e = np.array([0.40296894, 2.0, 4.98759174])
-    assert(M.shape[0] == M_e.shape[0])
+    assert (M_e.shape[0] == M_u.shape[0])
     for m, m_u in zip(M_e, M_u):
         assert_almost_equal(m, m_u, delta=0.1)
 
@@ -71,8 +145,8 @@ def test_serial_inference():
             assert_almost_equal(C_e[r, c], C_o[r, c], delta=0.5)
 
     samples = mvn.get_samples()
-    assert(samples.shape[0] == N)
-    assert(samples.shape[1] == 3)
+    assert (samples.shape[0] == N)
+    assert (samples.shape[1] == 3)
 
     mvn.clear()
 
