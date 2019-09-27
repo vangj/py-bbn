0a8dab12dbb972403da59549b4c57d5a3b6f6a3e
Jee Vang, Ph.D <vangjee@gmail.com>
2019-09-27 17:40:55 -0400

    enable copy

--- pybbn/graph/variable.py
+++ pybbn/graph/variable.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class Variable(object):
     """
     A variable.
@@ -12,6 +15,20 @@ class Variable(object):
         self.id = id
         self.name = name
         self.values = values
+
+    def __copy__(self):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
+
+    def __deepcopy__(self, memodict={}):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memodict[id(self)] = result
+        for k, v in self.__dict__.items():
+            setattr(result, k, deepcopy(v, memodict))
+        return result
 
     def __str__(self):
         return '{}|{}|{}'.format(self.id, self.name, self.values)
---
+++ tests/graph/test_variable.py
@@ -0,0 +1,51 @@
+import copy
+
+from nose import with_setup
+
+from pybbn.graph.variable import Variable
+
+
+def setup():
+    """
+    Setup.
+    :return: None.
+    """
+    pass
+
+
+def teardown():
+    """
+    Teardown.
+    :return: None.
+    """
+    pass
+
+
+@with_setup(setup, teardown)
+def test_copy():
+    lhs = Variable(0, 'a', ['t', 'f'])
+    rhs = copy.copy(lhs)
+
+    assert lhs.id == rhs.id
+    assert lhs.name == rhs.name
+    assert len(lhs.values) == len(rhs.values)
+    for lhs_v, rhs_v in zip(lhs.values, rhs.values):
+        assert lhs_v == rhs_v
+
+    lhs.values[0] = 'true'
+    assert lhs.values[0] == rhs.values[0]
+
+
+@with_setup(setup, teardown)
+def test_deep_copy():
+    lhs = Variable(0, 'a', ['t', 'f'])
+    rhs = copy.deepcopy(lhs)
+
+    assert lhs.id == rhs.id
+    assert lhs.name == rhs.name
+    assert len(lhs.values) == len(rhs.values)
+    for lhs_v, rhs_v in zip(lhs.values, rhs.values):
+        assert lhs_v == rhs_v
+
+    lhs.values[0] = 'true'
+    assert lhs.values[0] != rhs.values[0]
