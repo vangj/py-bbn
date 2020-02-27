cd53666af876f074d8b8bb9139da7b0e5be892c3
Jee Vang, Ph.D <vangjee@gmail.com>
2020-02-27 01:15:19 -0500

    save huang

--- profile/generate-and-save.py
+++ profile/generate-and-save.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 from pybbn.generator.bbngenerator import generate_bbn_to_file
+from pybbn.graph.dag import BbnUtil, Bbn
 
 np.random.seed(37)
 
 generate_bbn_to_file(900, 'singly-bbn.csv', 'singly', 10)
 generate_bbn_to_file(900, 'multi-bbn.csv', 'multi', 10)
+
+bbn = BbnUtil.get_huang_graph()
+Bbn.to_csv(bbn, 'huang.csv')
