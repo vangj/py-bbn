1e61fbf98d46842f2c83199aef5f452d6804b2e9
Jee Vang, Ph.D <vangjee@gmail.com>
2020-03-21 10:34:51 -0400

    assert

--- tests/pptc/test_transformer.py
+++ tests/pptc/test_transformer.py
@@ -37,6 +37,35 @@ def test_transformer():
 
     join_tree = Transformer.transform(cliques)
 
-    # assert later
-    # print(join_tree)
-    # assert 1 == 2
+    e_nodes = set([
+        '(d,e,f)',
+        '(e,g,h)',
+        '(c,e,g)',
+        '(a,b,c)',
+        '(b,c,d)',
+        '(c,d,e)',
+        '|(a,b,c) -- b,c -- (b,c,d)|',
+        '|(b,c,d) -- c,d -- (c,d,e)|',
+        '|(c,e,g) -- c,e -- (c,d,e)|',
+        '|(d,e,f) -- d,e -- (c,d,e)|',
+        '|(e,g,h) -- e,g -- (c,e,g)|'
+    ])
+
+    e_edges = set([
+        '(a,b,c)--|(a,b,c) -- b,c -- (b,c,d)|--(b,c,d)',
+        '(b,c,d)--|(b,c,d) -- c,d -- (c,d,e)|--(c,d,e)',
+        '(c,e,g)--|(c,e,g) -- c,e -- (c,d,e)|--(c,d,e)',
+        '(d,e,f)--|(d,e,f) -- d,e -- (c,d,e)|--(c,d,e)',
+        '(e,g,h)--|(e,g,h) -- e,g -- (c,e,g)|--(c,e,g)'
+    ])
+
+    o_nodes = [str(n) for n in join_tree.get_nodes()]
+    o_edges = [str(e) for e in join_tree.get_edges()]
+
+    assert len(e_nodes) == len(o_nodes)
+    for n in e_nodes:
+        assert n in o_nodes
+
+    assert len(e_edges) == len(o_edges)
+    for e in e_edges:
+        assert e in o_edges
