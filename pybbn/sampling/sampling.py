13bc2be1160538ca4b06044f7e1fc74ad6917447
Jee Vang, Ph.D <vangjee@gmail.com>
2020-05-22 01:20:04 -0400

    implement logic sampling

--- pybbn/sampling/sampling.py
+++ pybbn/sampling/sampling.py
@@ -1,12 +1,18 @@
 import itertools
 import bisect
 import numpy as np
+from collections import namedtuple
+from functools import cmp_to_key
+from numpy.random import uniform
+
+SortableNode = namedtuple('SortableNode', 'node_id parent_ids')
 
 
 class Table(object):
     def __init__(self, node, parents=[]):
         self.node = node
         self.parents = sorted(parents, key=lambda n: n.id)
+        self.parent_ids = [p.id for p in parents]
 
         if len(parents) == 0:
             self.probs = np.array([node.probs]).cumsum()
@@ -17,13 +23,23 @@ class Table(object):
             n = len(keys)
 
             probs = [node.probs[i:i + n] for i in range(0, len(node.probs), n)]
+            probs = [np.array(p).cumsum() for p in probs]
             self.probs = {k: p for k, p in zip(keys, probs)}
+
+    def get_value(self, prob, sample=None):
+        if not self.has_parents():
+            index = bisect.bisect(self.probs, prob)
+        else:
+            k = ','.join([f'{i}={sample[i]}' for i in self.parent_ids])
+            probs = self.probs[k]
+            index = bisect.bisect(probs, prob)
+        return self.node.variable.values[index]
 
     def has_parents(self):
         return self.parents is not None and len(self.parents) > 0
 
 
-class LogicSampling(object):
+class LogicSampler(object):
     """
     Logic sampling with rejection.
     """
@@ -34,19 +50,47 @@ class LogicSampling(object):
         :param bbn: BBN.
         """
         self.bbn = bbn
+        self.nodes = self.__topological_sort__()
+        self.tables = self.__get_tables__()
 
-    def __initialize__(self):
-        def get_parents(n):
-            return [self.bbn.get_node(pa_id) for pa_id in self.bbn.get_parents_ordered(n.id)]
+    def __get_tables__(self):
+        tables = [node for node in self.bbn.get_nodes()]
+        tables = [(node, self.bbn.get_parents_ordered(node.id)) for node in tables]
+        tables = [(node, [self.bbn.get_node(pa_id) for pa_id in parents]) for node, parents in tables]
+        tables = {node.id: Table(node, parents) for node, parents in tables}
+        return tables
 
-        [[node] + get_parents(node) for node in self.bbn.get_nodes()]
+    def __topological_sort__(self):
+        nodes = [SortableNode(node.id, set(self.bbn.get_parents_ordered(node.id))) for node in self.bbn.get_nodes()]
+        id_sorter = lambda x, y: -1 if x < y else 1 if x > y else 0
+        pa_sorter = lambda x, y: -1 if x.node_id in y.parent_ids else 1 if y.node_id in x.parent_ids else id_sorter(x.node_id, y.node_id)
+        nodes = sorted(nodes, key=cmp_to_key(pa_sorter))
+        nodes = [n.node_id for n in nodes]
+        return nodes
 
-    def get_samples(self, n_samples=100):
+    def get_samples(self, n_samples=100, seed=37):
         """
         Gets the samples.
 
         :param n_samples: Number of samples.
+        :param seed: Seed (default=37).
         :return: Samples.
         """
+        np.random.seed(seed)
+        n = len(self.nodes)
         samples = []
+        while True:
+            sample = {}
+
+            probs = uniform(0.0, 1.0, size=n)
+            for p, node_id in zip(probs, self.nodes):
+                table = self.tables[node_id]
+                val = table.get_value(p, sample=sample)
+                sample[node_id] = val
+
+            samples.append(sample)
+
+            if len(samples) == n_samples:
+                break
+
         return samples
--- tests/sampling/test_sampling.py
+++ tests/sampling/test_sampling.py
@@ -1,10 +1,13 @@
 from nose import with_setup
 import numpy as np
+import pandas as pd
 from numpy.testing import assert_almost_equal
 
+from pybbn.graph.dag import BbnUtil, Bbn
+from pybbn.graph.edge import Edge, EdgeType
 from pybbn.graph.node import BbnNode
 from pybbn.graph.variable import Variable
-from pybbn.sampling.sampling import Table
+from pybbn.sampling.sampling import Table, LogicSampler
 
 
 def setup():
@@ -30,3 +33,140 @@ def test_table():
 
     assert not table.has_parents()
     assert_almost_equal(table.probs, np.array([0.5, 1.0]))
+    assert 'on' == table.get_value(0.4)
+    assert 'off' == table.get_value(0.6)
+
+
+@with_setup(setup, teardown)
+def test_pa_ch_table():
+    a = BbnNode(Variable(0, 'a', ['on', 'off']), [0.5, 0.5])
+    b = BbnNode(Variable(1, 'b', ['on', 'off']), [0.5, 0.5, 0.4, 0.6])
+    table = Table(b, parents=[a])
+
+    assert table.has_parents()
+    lhs = np.array(list(table.probs.values()))
+    rhs = np.array([[0.5, 1.0], [0.4, 1.0]])
+    assert_almost_equal(lhs, rhs)
+    assert 'on' == table.get_value(0.4, sample={0: 'on'})
+    assert 'off' == table.get_value(0.7, sample={0: 'on'})
+    assert 'on' == table.get_value(0.3, sample={0: 'off'})
+    assert 'off' == table.get_value(0.6, sample={0: 'off'})
+
+
+@with_setup(setup, teardown)
+def test_toplogical_sort_huang():
+    bbn = BbnUtil.get_huang_graph()
+    sampler = LogicSampler(bbn)
+
+    assert_almost_equal([0, 1, 2, 3, 4, 5, 6, 7], sampler.nodes)
+
+
+@with_setup(setup, teardown)
+def test_toplogical_sort_reversed():
+    a = BbnNode(Variable(0, 'a', ['on', 'off']), [0.7, 0.3, 0.2, 0.8])
+    b = BbnNode(Variable(1, 'b', ['on', 'off']), [0.5, 0.5, 0.4, 0.6])
+    c = BbnNode(Variable(2, 'c', ['on', 'off']), [0.5, 0.5])
+
+    bbn = Bbn() \
+        .add_node(a) \
+        .add_node(b) \
+        .add_node(c) \
+        .add_edge(Edge(c, b, EdgeType.DIRECTED)) \
+        .add_edge(Edge(b, a, EdgeType.DIRECTED))
+
+    sampler = LogicSampler(bbn)
+
+    assert_almost_equal([2, 1, 0], sampler.nodes)
+
+
+@with_setup(setup, teardown)
+def test_toplogical_sort_mixed():
+    a = BbnNode(Variable(0, 'a', ['on', 'off']), [0.7, 0.3, 0.2, 0.8])
+    b = BbnNode(Variable(1, 'b', ['on', 'off']), [0.5, 0.5])
+    c = BbnNode(Variable(2, 'c', ['on', 'off']), [0.5, 0.5, 0.4, 0.6])
+
+    bbn = Bbn() \
+        .add_node(a) \
+        .add_node(b) \
+        .add_node(c) \
+        .add_edge(Edge(b, a, EdgeType.DIRECTED)) \
+        .add_edge(Edge(b, c, EdgeType.DIRECTED))
+
+    sampler = LogicSampler(bbn)
+
+    assert_almost_equal([1, 0, 2], sampler.nodes)
+
+
+@with_setup(setup, teardown)
+def test_sampler_tables():
+    a = BbnNode(Variable(0, 'a', ['on', 'off']), [0.5, 0.5])
+    b = BbnNode(Variable(1, 'b', ['on', 'off']), [0.5, 0.5, 0.4, 0.6])
+    c = BbnNode(Variable(2, 'c', ['on', 'off']), [0.7, 0.3, 0.2, 0.8])
+
+    bbn = Bbn() \
+        .add_node(a) \
+        .add_node(b) \
+        .add_node(c) \
+        .add_edge(Edge(a, b, EdgeType.DIRECTED)) \
+        .add_edge(Edge(b, c, EdgeType.DIRECTED))
+
+    sampler = LogicSampler(bbn)
+
+    assert_almost_equal([0, 1, 2], sampler.nodes)
+
+    tables = sampler.tables
+    assert 3 == len(tables)
+    assert 0 in tables
+    assert 1 in tables
+    assert 2 in tables
+
+    lhs = np.array(tables[0].probs)
+    rhs = np.array([0.5, 1.0])
+    assert_almost_equal(lhs, rhs)
+
+    lhs = np.array(list(tables[1].probs.values()))
+    rhs = np.array([[0.5, 1.0], [0.4, 1.0]])
+    assert_almost_equal(lhs, rhs)
+
+    lhs = np.array(list(tables[2].probs.values()))
+    rhs = np.array([[0.7, 1.0], [0.2, 1.0]])
+    assert_almost_equal(lhs, rhs)
+
+
+@with_setup(setup, teardown)
+def test_sampling():
+    a = BbnNode(Variable(0, 'a', ['on', 'off']), [0.5, 0.5])
+    b = BbnNode(Variable(1, 'b', ['on', 'off']), [0.5, 0.5, 0.4, 0.6])
+    c = BbnNode(Variable(2, 'c', ['on', 'off']), [0.7, 0.3, 0.2, 0.8])
+
+    bbn = Bbn() \
+        .add_node(a) \
+        .add_node(b) \
+        .add_node(c) \
+        .add_edge(Edge(a, b, EdgeType.DIRECTED)) \
+        .add_edge(Edge(b, c, EdgeType.DIRECTED))
+
+    sampler = LogicSampler(bbn)
+
+    n_samples = 10000
+    samples = pd.DataFrame(sampler.get_samples(n_samples=n_samples, seed=37))
+    samples.columns = ['a', 'b', 'c']
+
+    assert n_samples == samples.shape[0]
+    assert 3 == samples.shape[1]
+
+    s_a = samples.a.value_counts()
+    s_b = samples.b.value_counts()
+    s_c = samples.c.value_counts()
+
+    s_a = s_a / s_a.sum()
+    s_b = s_b / s_b.sum()
+    s_c = s_c / s_c.sum()
+
+    s_a = s_a.sort_index().values
+    s_b = s_b.sort_index().values
+    s_c = s_c.sort_index().values
+
+    assert_almost_equal(s_a, np.array([0.4985, 0.5015]))
+    assert_almost_equal(s_b, np.array([0.5502, 0.4498]))
+    assert_almost_equal(s_c, np.array([0.5721, 0.4279]))
