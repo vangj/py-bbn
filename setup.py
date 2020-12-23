653370deeae511bd94b72fdff4e44ad0e6161023
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-23 13:25:44 -0500

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pybbn',
-    version='3.0.0',
+    version='3.1.0',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
