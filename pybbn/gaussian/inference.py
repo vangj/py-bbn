846c291480bb166743ebf80d79a87c89cca9bd9d
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-25 14:59:39 -0500

    refactor

--- pybbn/gaussian/inference.py
+++ pybbn/gaussian/inference.py
@@ -86,24 +86,7 @@ class GaussianInference(object):
         :param observation: Observation value.
         :return: GaussianInference.
         """
-        z_index = [self.I[name]]
-        y_index = [i for i in range(self.E.shape[1]) if i not in z_index]
-
-        m_Z = np.array([m for i, m in enumerate(self.M) if i in z_index])
-        m_Y = np.array([m for i, m in enumerate(self.M) if i in y_index])
-
-        z = np.array([observation])
-
-        S_YZ = self.E[y_index][:, z_index]
-        S_ZZ = np.linalg.inv(self.E[z_index][:, z_index])
-        S_YY = self.E[y_index][:, y_index]
-
-        H = [h for i, h in enumerate(self.H) if h != name]
-        M = m_Y - S_YZ.dot(S_ZZ).dot(z - m_Z)
-        E = S_YY - S_YZ.dot(S_ZZ).dot(S_YZ.T)
-        meta = {**self.meta, **{name: observation}}
-
-        return GaussianInference(H, M, E, meta)
+        return self.do_inferences([(name, observation)])
 
     def do_inferences(self, observations):
         """
